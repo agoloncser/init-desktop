6f7e662b3c9d72781951c408a25a3c2efbcabe0f
Attila Gölöncsér <attila.goloncser@blackbelt.hu>
2020-06-16 17:56:46 +0200

    Making it faster to debug

--- build_Linux.sh
+++ build_Linux.sh
@@ -5,4 +5,8 @@ set -xv
 cat Dockerfile_${FROM_DISTRO}.template | envsubst > Dockerfile
 docker build . --tag travis
 docker images
-docker run -d --privileged --name travis -v /sys/fs/cgroup:/sys/fs/cgroup:ro travis && docker exec --tty travis env TERM=vt100 python --version && docker exec --tty travis env TERM=vt100 ansible --version
+docker ps
+docker run -d --privileged --name travis -v /sys/fs/cgroup:/sys/fs/cgroup:ro travis
+docker ps
+docker exec --tty travis env TERM=vt100 python --version
+docker exec --tty travis env TERM=vt100 ansible --version

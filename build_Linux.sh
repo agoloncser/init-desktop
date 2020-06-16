a383e7618ff7f6103e96345cc1ed1a9ce60cd162
Attila Gölöncsér <attila.goloncser@blackbelt.hu>
2020-06-16 16:52:31 +0200

    Show ps after starting image

--- build_Linux.sh
+++ build_Linux.sh
@@ -5,7 +5,7 @@ set -xv
 cat Dockerfile_${FROM_DISTRO}.template | envsubst > Dockerfile
 docker build . --tag travis
 docker images
-docker ps
 docker run -d --privileged --name travis -v /sys/fs/cgroup:/sys/fs/cgroup:ro travis
+docker ps
 docker exec --tty travis env TERM=vt100 python --version
 docker exec --tty travis env TERM=vt100 ansible --version

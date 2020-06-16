07395eac79ac566622706fb900ee39e0dd61b024
Attila Gölöncsér <attila.goloncser@blackbelt.hu>
2020-06-16 18:33:19 +0200

    Wait a bit and show ps again

--- build_Linux.sh
+++ build_Linux.sh
@@ -8,5 +8,7 @@ docker images
 docker ps
 docker run -d --privileged --name travis -v /sys/fs/cgroup:/sys/fs/cgroup:ro travis
 docker ps
-docker exec --tty travis env TERM=vt100 python --version
-docker exec --tty travis env TERM=vt100 ansible --version
+sleep 30
+docker ps
+# docker exec --tty travis env TERM=vt100 python --version
+# docker exec --tty travis env TERM=vt100 ansible --version

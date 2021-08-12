6627341150135f13998bfdfbfa7c74d8f3f687ec
Attila GOLONCSER <agl@kxxp.net>
2021-08-12 18:38:04 +0200

    Setup FreeBSD desktop #minor

--- scripts/00_freebsd.sh
+++ scripts/00_freebsd.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-sudo pkg update
-sudo pkg install -y git curl python38 py38-ansible gnupg password-store tmux openssh-portable py38-pipenv
+pkg update
+pkg install -y git curl python38 py38-ansible gnupg password-store tmux openssh-portable py38-pipenv
+
+# gnome
+pkg install -y gnome3-lite xorg
+cat <<EOF >> /etc/fstab
+proc           /proc       procfs  rw  0   0
+EOF
+sysctl dbus_enable="YES"
+sysctl gdm_enable="YES"
+sysctl gnome_enable="YES"

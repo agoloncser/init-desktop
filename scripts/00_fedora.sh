0c9548773d979f0070248cc84cb790cb788d5199
Attila GOLONCSER <agl@kxxp.net>
2021-08-12 18:24:19 +0200

    Add some more initial packages #patch

--- scripts/00_fedora.sh
+++ scripts/00_fedora.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -o errexit
 sudo dnf upgrade -y
-sudo dnf install -y git curl python3 ansible gnupg2 pass tmux
+sudo dnf install -y git curl python3 ansible gnupg2 pass tmux pipenv
--- scripts/00_ubuntu.sh
+++ scripts/00_ubuntu.sh
@@ -2,4 +2,4 @@
 set -o errexit
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt-get install -y git python3 curl ansible gnupg2 pass tmux
+sudo apt-get install -y git python3 curl ansible gnupg2 pass tmux pipenv scdaemon pcscd

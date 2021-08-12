f0178daf5e483c7e4bf0e8bca2de7a9c8414d683
Attila GOLONCSER <agl@kxxp.net>
2021-08-12 18:25:23 +0200

    Add pipenv to FreeBSD

--- scripts/00_freebsd.sh
+++ scripts/00_freebsd.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo pkg update
-sudo pkg install -y git curl python38 py38-ansible gnupg password-store tmux openssh-portable
+sudo pkg install -y git curl python38 py38-ansible gnupg password-store tmux openssh-portable py38-pipenv

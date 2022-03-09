5843188a30455a4d6a8c8e70877fc3dde4718975
Attila GOLONCSER <agl@kxxp.net>
2022-03-09 10:28:57 +0100

    fix: Add fish shell on linuxes

--- src/00_fedora.sh
+++ src/00_fedora.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -o errexit
 sudo dnf upgrade -y
-sudo dnf install -y git curl python3 ansible gnupg2 tmux pipenv gnupg-pkcs11-scd pcsc-lite-ccid pcsc-lite xz rsync bash pass pass-otp
+sudo dnf install -y git curl python3 ansible gnupg2 tmux pipenv gnupg-pkcs11-scd pcsc-lite-ccid pcsc-lite xz rsync bash pass pass-otp fish
 
 sudo systemctl enable pcscd
 sudo systemctl start pcscd

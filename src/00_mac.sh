0491e31e6d27adbde265f05797a3cfa377092436
Attila GOLONCSER <agl@kxxp.net>
2022-03-25 16:06:49 +0100

    feat: Install homebrew locally

--- src/00_mac.sh
+++ src/00_mac.sh
@@ -2,12 +2,14 @@
 set -o errexit
 
 # installing homebrew
-if [ ! -x /usr/local/bin/brew ] ; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+if [ ! -x $HOME/.brew/bin/brew ] ; then
+    git clone https://github.com/Homebrew/brew.git $HOME/.brew
+    $HOME/.brew/bin/brew tap homebrew/core
 else
     echo Already installed
 fi
 
+export PATH=$HOME/.brew/bin:$PATH
 brew update
 brew upgrade
 brew install git ansible gnupg curl pass tmux openssh xz rsync pass pass-otp mas fish

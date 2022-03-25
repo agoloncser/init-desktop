72b48e784e046f29c19572708591ff5adba16613
Attila GOLONCSER <agl@kxxp.net>
2022-03-25 16:54:46 +0100

    Revert "feat: Install homebrew locally"
    This reverts commit 0491e31e6d27adbde265f05797a3cfa377092436.

--- src/00_mac.sh
+++ src/00_mac.sh
@@ -2,14 +2,12 @@
 set -o errexit
 
 # installing homebrew
-if [ ! -x $HOME/.brew/bin/brew ] ; then
-    git clone https://github.com/Homebrew/brew.git $HOME/.brew
-    $HOME/.brew/bin/brew tap homebrew/core
+if [ ! -x /usr/local/bin/brew ] ; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 else
     echo Already installed
 fi
 
-export PATH=$HOME/.brew/bin:$PATH
 brew update
 brew upgrade
 brew install git ansible gnupg curl pass tmux openssh xz rsync pass pass-otp mas fish

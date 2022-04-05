2afc88d94730bb688a4f91a1f5084a5297f9b2cf
Attila GOLONCSER <agl@kxxp.net>
2022-04-05 13:20:32 +0200

    Call homebrew correctly

--- src/00_mac.sh
+++ src/00_mac.sh
@@ -5,11 +5,15 @@ dir="$HOME/.homebrew"
 
 # installing homebrew
 if [ ! -x "$dir/bin/brew" ] ; then
-    git clone https://github.com/Homebrew/brew $dir
+    git clone https://github.com/Homebrew/brew "$dir"
 else
     echo Already installed
 fi
 
-"$dir/bin/brew" update
-"$dir/bin/brew" upgrade
-"$dir/bin/brew" install git ansible gnupg curl pass tmux openssh xz rsync pass pass-otp mas fish
+eval "$($dir/bin/brew shellenv)"
+brew update --force --quiet
+chmod -R go-w "$(brew --prefix)/share/zsh"
+
+brew update
+brew upgrade
+brew install git ansible gnupg curl pass tmux openssh xz rsync pass pass-otp mas fish

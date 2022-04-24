3522b664bbb348f02fe5a7a4748fbff085fc9c53
Attila GOLONCSER <agl@kxxp.net>
2022-04-24 17:41:50 +0200

    fix: Add wl-clipboard

--- src/00_ubuntu.sh
+++ src/00_ubuntu.sh
@@ -2,6 +2,6 @@
 set -o errexit
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt-get install -y git python3 curl ansible gnupg2 pass tmux pipenv scdaemon pcscd xz-utils bash rsync pass-extension-otp fish
+sudo apt-get install -y git python3 curl ansible gnupg2 pass tmux pipenv scdaemon pcscd xz-utils bash rsync pass-extension-otp fish wl-clipboard
 sudo systemctl enable pcscd
 sudo systemctl start pcscd

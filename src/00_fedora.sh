0a10a5766ec23c777d046008215c31b612039152
Attila GOLONCSER <agl@kxxp.net>
2022-04-25 14:13:29 +0200

    fix: Update packages

--- src/00_fedora.sh
+++ src/00_fedora.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -o errexit
 sudo dnf upgrade -y
-sudo dnf install -y git curl python3 ansible gnupg2 tmux pipenv gnupg-pkcs11-scd pcsc-lite-ccid pcsc-lite xz rsync bash pass pass-otp fish
-
+sudo dnf install -y git curl python3 ansible gnupg2 tmux pipenv gnupg-pkcs11-scd pcsc-lite-ccid pcsc-lite xz rsync bash pass pass-otp fish wl-clipboard freerdp2
 sudo systemctl enable pcscd
 sudo systemctl start pcscd

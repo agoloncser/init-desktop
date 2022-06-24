edd82dfcc35702dc34dcebb0163099726212fbb2
Attila GOLONCSER <agl@kxxp.net>
2022-06-24 17:05:38 +0200

    fix: Ubuntu enable repos

--- src/00_ubuntu.sh
+++ src/00_ubuntu.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -o errexit
+sudo add-apt-repository universe
+sudo add-apt-repository multiverse
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt-get install -y git python3 curl ansible gnupg2 pass tmux pipenv scdaemon pcscd xz-utils bash rsync pass-extension-otp fish

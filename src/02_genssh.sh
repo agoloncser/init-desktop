aa5b14dd117b85c890ca3a4fef2bb47613e0bd48
Attila GOLONCSER <agl@kxxp.net>
2022-08-30 14:15:19 +0200

    fix: Add help and minor fixes

--- src/02_genssh.sh
+++ src/02_genssh.sh
@@ -1,25 +1,34 @@
 #!/bin/sh
 
-set -eu
 set -xv
 
-while getopts k:l: options
+while getopts l:k: options
 do
     case $options in
         k) key_usage=$OPTARG ;;
-        l) passphrase_lenght=$OPTARG ;;
+        l) passphrase_length=$OPTARG ;;
         *) echo "Invalid options" ; exit 1
     esac
 done
 
+_help(){
+cat <<EOF
+
+-l INT .... passphrase length (e.g. '64')
+-k NAME ... name the usage  (e.g. 'github')
+
+EOF
+exit 1
+}
+
 if [ -z "$key_usage" ] ; then
-    echo "Key usage must be set (mmegh|github|work)."
-    exit 1
+    _help
 fi
-if [ -z "$passphrase_lenght" ] ; then
-    echo "Passphrase lenght must be set (integer)."
-    exit 1
+if [ -z "$passphrase_length" ] ; then
+    _help
 fi
+
+set -eu
 
 MY_HOSTNAME=$(hostname | perl -F'\.' -lane 'print $F[0]')
 
@@ -27,9 +36,9 @@ KEYNAME="ed25519-${key_usage}-${MY_HOSTNAME}-${USER}"
 PASSPHRASE_LOCATION="ssh/${KEYNAME}/passphrase"
 PUBKEY_LOCATION="ssh/${KEYNAME}/pubkey"
 
-pass generate --no-symbols "${PASSPHRASE_LOCATION}" "$passphrase_lenght"
+pass generate --no-symbols "${PASSPHRASE_LOCATION}" "$passphrase_length"
 mkdir -p "$HOME/.ssh" || true
 chmod 0700 "$HOME/.ssh"
-ssh-keygen -t ed25519 -C "[$KEYNAME-$(date +%Y%m%d-%H%M%S)]" -f "$HOME/.ssh/${KEYNAME}" -P "$(pass "${PASSPHRASE_LOCATION}")"
+ssh-keygen -t ed25519 -C "[${KEYNAME}-$(date +%Y%m%d-%H%M%S)]" -f "$HOME/.ssh/${KEYNAME}" -P "$(pass "${PASSPHRASE_LOCATION}")"
 cat "$HOME/.ssh/${KEYNAME}.pub" | pass insert -m "$PUBKEY_LOCATION"
 cat "$HOME/.ssh/${KEYNAME}.pub"

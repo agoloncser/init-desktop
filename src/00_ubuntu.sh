15500d25810d62e2e2a35a9755d6f08d4c6323cc
Attila GOLONCSER <agl@kxxp.net>
2023-03-21 12:15:00 +0100

    fix: Add gconf2 package to Ubuntu

--- src/00_ubuntu.sh
+++ src/00_ubuntu.sh
@@ -4,6 +4,6 @@ sudo add-apt-repository universe
 sudo add-apt-repository multiverse
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt-get install -y git python3 curl ansible gnupg2 pass tmux pipenv scdaemon pcscd xz-utils bash rsync pass-extension-otp fish wl-clipboard freerdp2-x11 pipenv
+sudo apt-get install -y git python3 curl ansible gnupg2 pass tmux pipenv scdaemon pcscd xz-utils bash rsync pass-extension-otp fish wl-clipboard freerdp2-x11 pipenv gconf2
 sudo systemctl enable pcscd
 sudo systemctl start pcscd

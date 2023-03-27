4b4b924086bef292314157de4060e37aad6087df
Attila GOLONCSER <agl@kxxp.net>
2023-03-27 14:32:03 +0200

    feat(genssh): Add support for directory and key customization

--- src/02_genssh.sh
+++ src/02_genssh.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-while getopts l:k: options
+flag_H=""
+flag_U=""
+while getopts l:k:d:HU options
 do
     case $options in
         k) key_usage=$OPTARG ;;
         l) passphrase_length=$OPTARG ;;
+        d) key_directory=$OPTARG ;;
+        H) flag_H=1 ;;
+        U) flag_U=1 ;;
         *) echo "Invalid options" ; exit 1
     esac
 done
@@ -14,6 +19,7 @@ cat <<EOF
 
 -l INT .... passphrase length (e.g. '64')
 -k NAME ... name the usage  (e.g. 'github')
+-d DIR .... directory to save the key (optional, default; '$HOME/.ssh')
 
 Only ED25519 keys are supported.
 
@@ -34,15 +40,24 @@ fi
 
 set -eu
 
-MY_HOSTNAME=$(hostname | perl -F'\.' -lane 'print $F[0]')
+KEYNAME="ed25519-${key_usage}"
+if [ -z "$flag_H" ] ; then
+    MY_HOSTNAME=$(hostname | perl -F'\.' -lane 'print $F[0]')
+    KEYNAME="${KEYNAME}-${MY_HOSTNAME}"
+fi
+if [ -z "$flag_U" ] ; then
+    KEYNAME="${KEYNAME}-${USER}"
+fi
 
-KEYNAME="ed25519-${key_usage}-${MY_HOSTNAME}-${USER}"
 PASSPHRASE_LOCATION="ssh/${KEYNAME}/passphrase"
-PUBKEY_LOCATION="ssh/${KEYNAME}/pubkey"
+# PUBKEY_LOCATION="ssh/${KEYNAME}/pubkey"
+
+# this is optional so we can give a default
+key_directory=${key_directory:="${HOME}/.ssh/"}
 
 pass generate --no-symbols "${PASSPHRASE_LOCATION}" "$passphrase_length"
-mkdir -p "$HOME/.ssh" || true
-chmod 0700 "$HOME/.ssh"
+mkdir -p "$key_directory" || true
+chmod 0700 "$key_directory"
 echo 'Touch the device...'
-ssh-keygen -t ed25519 -C "${KEYNAME}-$(date +%Y%m%d-%H%M%S)" -f "$HOME/.ssh/${KEYNAME}" -P "$(pass "${PASSPHRASE_LOCATION}")"
-cat "$HOME/.ssh/${KEYNAME}.pub"
+ssh-keygen -t ed25519 -C "${KEYNAME}-$(date +%Y%m%d-%H%M%S)" -f "${key_directory}/${KEYNAME}" -P "$(pass "${PASSPHRASE_LOCATION}")"
+cat "${key_directory}/${KEYNAME}.pub"

162b217a73f90913bf4416959dac7cbab58635a9
Attila GOLONCSER <agl@kxxp.net>
2023-03-27 14:47:45 +0200

    feat(genssh): Add pubkey to pass, control pass location

--- src/02_genssh.sh
+++ src/02_genssh.sh
@@ -10,6 +10,7 @@ do
         d) key_directory=$OPTARG ;;
         H) flag_H=1 ;;
         U) flag_U=1 ;;
+        B) PASS_BASE_DIRECTORY=$OPTARG ;;
         *) echo "Invalid options" ; exit 1
     esac
 done
@@ -40,6 +41,7 @@ fi
 
 set -eu
 
+PASS_BASE_DIRECTORY=${PASS_BASE_DIRECTORY:="ssh"}
 KEYNAME="ed25519-${key_usage}"
 if [ -z "$flag_H" ] ; then
     MY_HOSTNAME=$(hostname | perl -F'\.' -lane 'print $F[0]')
@@ -49,8 +51,8 @@ if [ -z "$flag_U" ] ; then
     KEYNAME="${KEYNAME}-${USER}"
 fi
 
-PASSPHRASE_LOCATION="ssh/${KEYNAME}/passphrase"
-# PUBKEY_LOCATION="ssh/${KEYNAME}/pubkey"
+PASSPHRASE_LOCATION="${PASS_BASE_DIRECTORY}/${KEYNAME}/passphrase"
+PUBKEY_LOCATION="${PASS_BASE_DIRECTORY}/${KEYNAME}/pubkey"
 
 # this is optional so we can give a default
 key_directory=${key_directory:="${HOME}/.ssh/"}
@@ -60,4 +62,7 @@ mkdir -p "$key_directory" || true
 chmod 0700 "$key_directory"
 echo 'Touch the device...'
 ssh-keygen -t ed25519 -C "${KEYNAME}-$(date +%Y%m%d-%H%M%S)" -f "${key_directory}/${KEYNAME}" -P "$(pass "${PASSPHRASE_LOCATION}")"
+pass insert -m "$PUBKEY_LOCATION" < "${key_directory}/${KEYNAME}.pub"
+
+echo
 cat "${key_directory}/${KEYNAME}.pub"

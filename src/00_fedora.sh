9fd73d96cc454b52011bcfe0aa2e19eb27f3b84e
Attila GOLONCSER <agl@kxxp.net>
2023-05-15 16:29:49 +0200

    fix: Fix package list

--- src/00_fedora.sh
+++ src/00_fedora.sh
@@ -6,6 +6,6 @@ DESKTOP_INIT_SKIP_UPGRADE=${DESKTOP_INIT_SKIP_UPGRADE:=""}
 if [ -z "$DESKTOP_INIT_SKIP_UPGRADE" ] ; then
     sudo dnf upgrade -y
 fi
-sudo dnf install -y git curl python3 ansible gnupg2 tmux pipenv gnupg-pkcs11-scd pcsc-lite-ccid pcsc-lite xz rsync bash pass pass-otp fish wl-clipboard freerdp2 pipenv
+sudo dnf install -y git curl python3 ansible gnupg2 tmux pipenv gnupg-pkcs11-scd pcsc-lite-ccid pcsc-lite xz rsync bash pass pass-otp fish wl-clipboard freerdp pipenv
 sudo systemctl enable pcscd
 sudo systemctl start pcscd

3616e370e6c60be09d64e1c3a49ab5ecc09da0e8
Attila GOLONCSER <agl@kxxp.net>
2023-05-17 11:09:11 +0200

    fix(genssh): Add NO_PASSPHRASE option, help and clarify variables

--- src/02_genssh.sh
+++ src/02_genssh.sh
@@ -2,20 +2,22 @@
 
 set -eu
 
-flag_H=""
-flag_U=""
+NO_HOSTNAME=""
+NO_USERNAME=""
 key_usage=""
 passphrase_length="64"
-while getopts l:k:d:HUB:C: options
+NO_PASSPHRASE=""
+while getopts P:l:k:d:HUB:C: options
 do
     case $options in
         k) key_usage=$OPTARG ;;
         l) passphrase_length=$OPTARG ;;
         d) key_directory=$OPTARG ;;
-        H) flag_H=1 ;;
-        U) flag_U=1 ;;
+        H) NO_HOSTNAME=1 ;;
+        U) NO_USERNAME=1 ;;
         B) PASS_BASE_DIRECTORY=$OPTARG ;;
         C) CA_NAME=$OPTARG ;;
+        P) NO_PASSPHRASE=1 ;;
         *) echo "Invalid options" ; exit 1
     esac
 done
@@ -27,6 +29,10 @@ cat <<EOF
 -k NAME ... name the usage  (e.g. 'github')
 -d DIR .... directory to save the key (optional, default; '$HOME/.ssh')
 -C CA ..... name of the CA to use (optional)
+-H ........ do not add current hostname to key name (optional)
+-U ........ do not add current username to key name (optional)
+-B ........ set base directory in pass where to store the pubkey (optional)
+-P ........ do not create a key passphrase in pass but request interactively (optional)
 
 Only ED25519 keys are supported.
 
@@ -41,7 +47,7 @@ exit 1
 if [ -z "$key_usage" ] ; then
     _help
 fi
-if [ -z "$passphrase_length" ] ; then
+if [ -z "$passphrase_length" ] && [ -z "$NO_PASSPHRASE" ] ; then
     _help
 fi
 
@@ -49,11 +55,11 @@ set -eu
 
 PASS_BASE_DIRECTORY=${PASS_BASE_DIRECTORY:="ssh"}
 KEYNAME="ed25519-${key_usage}"
-if [ -z "$flag_H" ] ; then
+if [ -z "$NO_HOSTNAME" ] ; then
     MY_HOSTNAME=$(hostname | perl -F'\.' -lane 'print $F[0]')
     KEYNAME="${KEYNAME}-${MY_HOSTNAME}"
 fi
-if [ -z "$flag_U" ] ; then
+if [ -z "$NO_USERNAME" ] ; then
     KEYNAME="${KEYNAME}-${USER}"
 fi
 
@@ -68,11 +74,17 @@ fi
 # this is optional so we can give a default
 key_directory=${key_directory:="${HOME}/.ssh/"}
 
-pass generate --no-symbols "${PASSPHRASE_LOCATION}" "$passphrase_length"
+if [ -z "$NO_PASSPHRASE" ] ; then
+    pass generate --no-symbols "${PASSPHRASE_LOCATION}" "$passphrase_length"
+fi 
 mkdir -p "$key_directory" || true
 chmod 0700 "$key_directory"
-echo 'Touch the device...'
-ssh-keygen -t ed25519 -C "$KEY_COMMENT" -f "${key_directory}/${KEYNAME}" -P "$(pass "${PASSPHRASE_LOCATION}")"
+if [ -z "$NO_PASSPHRASE" ] ; then
+    echo 'Touch the device...'
+    ssh-keygen -t ed25519 -C "$KEY_COMMENT" -f "${key_directory}/${KEYNAME}" -P "$(pass "${PASSPHRASE_LOCATION}")"
+else
+    ssh-keygen -t ed25519 -C "$KEY_COMMENT" -f "${key_directory}/${KEYNAME}"
+fi
 pass insert -m "$PUBKEY_LOCATION" < "${key_directory}/${KEYNAME}.pub"
 
 echo

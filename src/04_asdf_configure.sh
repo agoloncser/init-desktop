2db744c19ec14c8e6e1621e9150fa463cec26477
Attila GOLONCSER <agl@kxxp.net>
2023-11-24 18:34:08 +0100

    feat: Install node packages
    Closes #41
    Closes #45
    Closes #35

--- src/04_asdf_configure.sh
+++ src/04_asdf_configure.sh
@@ -10,19 +10,13 @@ ASDF_DOWNLOAD_PATH="$(mktemp -d)"
 export ZSH_VERSION
 export ASDF_DOWNLOAD_PATH
 
-source "$ASDF_BIN"
+. "$ASDF_BIN"
 
 set +eu
 asdf plugin add ghq    https://github.com/kajisha/asdf-ghq.git || true
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git || true
 asdf plugin add python https://github.com/danhper/asdf-python.git || true
 set -eu
-
-cat <<EOF > "$HOME/.default-python-packages"
-ansible-core
-pipenv
-yamllint
-EOF
 
 grep '\.asdf\/asdf.sh' $HOME/.bashrc ||
 cat <<EOF >> $HOME/.bashrc
@@ -31,8 +25,22 @@ cat <<EOF >> $HOME/.bashrc
 EOF
 
 cp shared/asdf/.default-python-packages "$HOME/.default-python-packages"
+cp shared/asdf/.default-npm-packages "$HOME/.default-npm-packages"
 cp shared/asdf/.tool-versions "$HOME/.tool-versions"
 
+echo "Update asdf..."
 asdf update
 asdf plugin-update --all 
+
+echo "Install asdf tools..."
 asdf install
+
+echo "Update python packages..."
+pip install --upgrade pip
+pip install --user -r "$HOME/.default-python-packages"
+
+echo "Update npm packages..."
+npm update -g npm
+xargs npm update --global < "$HOME/.default-npm-packages"
+
+

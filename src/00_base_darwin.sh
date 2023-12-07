d59342e1a19d76b90be8087e30cb634420e14220
Attila GOLONCSER <agl@kxxp.net>
2023-12-07 15:41:46 +0100

    fix: Support both x86_64 and arm64 macOS
    Closes #36

--- src/00_base_darwin.sh
+++ src/00_base_darwin.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
-set -euxv
+set -eu
 
 DESKTOP_INIT_SKIP_UPGRADE=${DESKTOP_INIT_SKIP_UPGRADE:=""}
 
 # installing homebrew
-if [ ! -x /usr/local/bin/brew ] ; then
+HOMEBREW_BIN=/opt/homebrew/bin/brew
+case $(uname -a) in
+    *x86_64) HOMEBREW_BIN=/usr/local/bin/brew ;;
+    *arm64) HOMEBREW_BIN=/opt/homebrew/bin/brew ;;
+    *) exit 1 ;;
+esac
+
+if [ ! -x "$HOMEBREW_BIN" ] ; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 else
-    echo Already installed
+    echo Homebrew already installed
 fi
 
 brew update

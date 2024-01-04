95280683df0cdeb9caa83b436670628037712c71
Attila GOLONCSER <agl@kxxp.net>
2024-01-04 14:48:15 +0100

    fix: Do not interrupt scripts for input

--- src/03_asdf_deps_fedora.sh
+++ src/03_asdf_deps_fedora.sh
@@ -4,8 +4,8 @@ set -eu
 
 ASDF_BIN=~/.asdf/asdf.sh
 
-sudo dnf install curl git
+sudo dnf install -y curl git
 
 # https://github.com/pyenv/pyenv/wiki#suggested-build-environment
-sudo dnf install make gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel
+sudo dnf install -y make gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel
 

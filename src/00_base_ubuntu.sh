509d1fc9c910b25667c20c5616ba427ed283d404
Attila GOLONCSER <agl@kxxp.net>
2024-01-17 09:48:22 +0100

    fix: Prefer apt-get in scripts

--- src/00_base_ubuntu.sh
+++ src/00_base_ubuntu.sh
@@ -5,10 +5,10 @@ INSTALL_FAST=${INSTALL_FAST:=""}
 
 sudo add-apt-repository -y universe
 sudo add-apt-repository -y multiverse
-sudo apt update -y
+sudo apt-get update -y
 
 if [ -z "$INSTALL_FAST" ] ; then
-    sudo apt upgrade -y
+    sudo apt-get upgrade -y
 fi
 
 sudo apt-get install -y \
--- src/03_asdf_deps_ubuntu.sh
+++ src/03_asdf_deps_ubuntu.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-sudo apt install -y curl git
+sudo apt-get install -y curl git
 
 # https://github.com/pyenv/pyenv/wiki#suggested-build-environment
-sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev bash
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev bash

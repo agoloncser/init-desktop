2823d475913b459214f9e85bd357e991b8a0b466
Attila GOLONCSER <agl@kxxp.net>
2024-01-26 16:14:23 +0100

    feat: Optional github-cli installer

---
+++ src/10_gh.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#
+# https://github.com/cli/cli/blob/trunk/docs/install_linux.md#opensusesuse-linux-zypper
+
+set -eu
+
+echo "Installing Github CLI..."
+case $(uname -s) in
+    Linux)
+        . /etc/os-release
+        case $ID in
+            opensuse*)
+                sudo zypper addrepo https://cli.github.com/packages/rpm/gh-cli.repo
+                sudo zypper ref
+                sudo zypper install gh
+                ;;
+            ubuntu)
+                sudo apt update && sudo apt install curl -y
+                curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+                && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+                && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+                && sudo apt update \
+                && sudo apt install gh -y
+                ;;
+            fedora)
+                sudo dnf install 'dnf-command(config-manager)'
+                sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+                sudo dnf install gh
+                ;;
+            *)
+                echo "Unsupported distro."
+                exit 0 ;;
+
+        esac
+
+        sudo systemctl disable resilio-sync || true
+        systemctl --user enable resilio-sync
+        systemctl --user start resilio-sync
+        ;;
+    # Darwin) brew install --cask 1password ;;
+    *)
+         echo "Unsupported OS."
+         exit 0
+         ;;
+esac

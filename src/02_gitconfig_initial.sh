40528d902f7ac51956766541bed7969212f3630b
Attila GOLONCSER <agl@kxxp.net>
2024-02-11 16:57:47 +0100

    feat: Remove oauth configuration
    Favouring Github CLI.

--- src/02_gitconfig_initial.sh
+++ src/02_gitconfig_initial.sh
@@ -5,29 +5,3 @@ set -eu
 git config --global credential.helper cache
 git config --global alias.root "rev-parse --show-toplevel"
 git config --global ghq.root ~/src
-
-_configure_oauth(){
-    echo "Configure git-credential-oauth"
-    git credential-oauth configure
-}
-
-case $(uname -s) in
-    Darwin) _configure_oauth ;;
-    Linux)
-        . /etc/os-release
-        if [ -z "$ID" ] ; then
-            echo "ERROR: Cannot detect distro."
-        fi
-        case $ID in
-            fedora) _configure_oauth ;;
-            ubuntu)  echo "WARNING: setup the HTTPS authentication according to README.md manually."
-                exit 0
-                ;;
-            *)  echo "Unsupported OS: $ID"
-                ;;
-        esac
-    ;;
-    *) echo "Unsupported OS: $(uname -s)"
-       exit 1
-    ;;
-esac

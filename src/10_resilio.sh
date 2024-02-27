dc2d63ac79f92ab0d41d8026f0b9588092067fb3
Attila GOLONCSER <agl@kxxp.net>
2024-02-27 12:59:13 +0100

    fix: Fix resilio-sync automatic installation and startup

--- src/10_resilio.sh
+++ src/10_resilio.sh
@@ -15,24 +15,27 @@ case $(uname -s) in
             opensuse*)
                 sudo rpm --import https://linux-packages.resilio.com/resilio-sync/key.asc
                 sudo zypper ar --gpgcheck-allow-unsigned-repo -f https://linux-packages.resilio.com/resilio-sync/rpm/\$basearch resilio-sync || true
-                sudo zypper install resilio-sync
+                sudo zypper install -y resilio-sync
                 ;;
             ubuntu)
+                sudo apt-get update
                 sudo apt-get install -y wget
                 echo "deb http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free" | sudo tee /etc/apt/sources.list.d/resilio-sync.list
                 wget -qO- https://linux-packages.resilio.com/resilio-sync/key.asc | sudo tee /etc/apt/trusted.gpg.d/resilio-sync.asc > /dev/null 2>&1
+                sudo apt-get update
                 sudo apt-get install -y resilio-sync
                 ;;
             fedora)
                 sudo rpm --import https://linux-packages.resilio.com/resilio-sync/key.asc
                 printf "[resilio-sync]\nname=Resilio Sync\nbaseurl=https://linux-packages.resilio.com/resilio-sync/rpm/\$basearch\nenabled=1\ngpgcheck=1\n" | sudo tee /etc/yum.repos.d/resilio-sync.repo
-                sudo dnf install resilio-sync
+                sudo dnf install -y resilio-sync
                 ;;
             *)
                 echo "Unsupported distro."
                 exit 0 ;;
 
         esac
+        sudo systemctl stop resilio-sync
         sudo systemctl disable resilio-sync || true
         systemctl --user enable resilio-sync
         systemctl --user start resilio-sync
